0115bf2dc39dee3d7b08003707499740c6efe277
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2025-12-18 10:31:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors({
+  origin: [
+    "https://your-frontend-name.vercel.app"
+  ],
+  credentials: true
+}));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Futurely Backend is running 🚀");
+});
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
