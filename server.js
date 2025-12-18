2a5982d60c2ab366d877566ef33b7d11af530b57
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2025-12-18 20:16:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,15 +9,8 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "https://www.futurely.in",
-    "https://futurely.in",
-    "http://localhost:3000"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
+
 
 app.use(express.json());
 
@@ -31,3 +24,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
