fe5ac1a360cd381b55128af13809823c3261e805
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2025-12-22 04:55:34 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,12 @@
-import mongoose from "mongoose";
-
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 
-  // 🔐 Forgot password
   resetOTP: String,
   resetOTPExpiry: Date,
+  resetOTPSentAt: Date,
+  resetOTPAttempts: { type: Number, default: 0 },
 
   role: { type: String, default: "student" },
 }, { timestamps: true });
-
-
-export default mongoose.model("User", userSchema);
