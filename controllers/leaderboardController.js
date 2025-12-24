d7d5db746c83c55e2b986453183d76d105363619
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2025-12-24 10:10:55 +0530

    Update leaderboardController.js

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,46 +1,42 @@
 import PhysicsAttempt from "../models/PhysicsAttempt.js";
-import User from "../models/User.js";
 
 export const getPhysicsLeaderboard = async (req, res) => {
   try {
     const data = await PhysicsAttempt.aggregate([
-      {
-        $match: { isCorrect: true }
-      },
+      { $match: { isCorrect: true } },
       {
         $group: {
           _id: "$user",
-          correctCount: { $sum: 1 }
-        }
+          correct: { $sum: 1 },
+        },
       },
       {
         $lookup: {
           from: "users",
           localField: "_id",
           foreignField: "_id",
-          as: "user"
-        }
+          as: "user",
+        },
       },
       { $unwind: "$user" },
       {
         $project: {
           name: "$user.name",
-          points: { $multiply: ["$correctCount", 4] }
-        }
+          points: { $multiply: ["$correct", 4] },
+        },
       },
-      { $sort: { points: -1 } }
+      { $sort: { points: -1 } },
     ]);
 
-    // Add position
     const leaderboard = data.map((u, i) => ({
       position: i + 1,
       name: u.name,
-      points: u.points
+      points: u.points,
     }));
 
-    return res.json(leaderboard);
+    res.json(leaderboard);
   } catch (error) {
     console.error("LEADERBOARD ERROR:", error);
-    return res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Server error" });
   }
 };
