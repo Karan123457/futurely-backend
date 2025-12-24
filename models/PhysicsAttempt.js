22b27ea75345895dbd2fc75991965358de6dc9e2
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2025-12-24 08:40:12 +0530

    Update PhysicsAttempt.js

--- models/PhysicsAttempt.js
+++ models/PhysicsAttempt.js
@@ -6,7 +6,28 @@ const physicsAttemptSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
+    },import mongoose from "mongoose";
+
+const physicsAttemptSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
+    questionId: { type: String, required: true },
+    year: { type: String, required: true },
+    selectedIndex: { type: Number, required: true },
+    correctIndex: { type: Number, required: true },
+    isCorrect: { type: Boolean, required: true },
+    timeTaken: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+
+export default mongoose.model("PhysicsAttempt", physicsAttemptSchema);
+
 
     questionId: {
       type: String,
@@ -37,3 +58,4 @@ const physicsAttemptSchema = new mongoose.Schema(
 );
 
 export default mongoose.model("PhysicsAttempt", physicsAttemptSchema);
+
