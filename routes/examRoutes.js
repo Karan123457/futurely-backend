d48b59691c802ec4f5e75a805b62527cbf5fb070
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2025-12-26 10:20:17 +0530

    Delete routes directory

--- routes/examRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { savePhysicsAttempt } from "../controllers/examController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.post("/physics/attempt", authMiddleware, savePhysicsAttempt);
-
-export default router;
--- routes/physicsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authMiddleware from "../middleware/authMiddleware.js";
-import { savePhysicsAttempt } from "../controllers/physicsController.js";
-
-const router = express.Router();
-
-router.post("/attempt", authMiddleware, savePhysicsAttempt);
-
-export default router;
