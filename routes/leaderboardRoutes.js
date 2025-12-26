2f20b16883d1dfa82331f0913f6865b6faa248d7
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2025-12-26 06:29:40 +0530

    Update leaderboardRoutes.js

--- routes/leaderboardRoutes.js
+++ routes/leaderboardRoutes.js
@@ -1,12 +1,9 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import {
-  getPhysicsLeaderboard,
-  getOverallLeaderboard,
-} from "../controllers/leaderboardController.js";
+import { getOverallLeaderboard } from "../controllers/leaderboardController.js";
 
 const router = express.Router();
 
-router.get("/physics", authMiddleware, getPhysicsLeaderboard);
 router.get("/overall", authMiddleware, getOverallLeaderboard);
+
 export default router;
