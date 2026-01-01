86ec3a135c484f32d4231b4f440373c030e9d840
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2026-01-01 20:01:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/exam", examRoutes);
 app.use("/api/leaderboard", leaderboardRoutes);
 app.use("/api/questions", questionRoutes);
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
